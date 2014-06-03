c8fe87034ff4ec1aa59fc47592c00eb3f2419f89
Andrew Tamura <andrewtamura@gmail.com>
2014-06-02 22:10:22 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,42 @@
+Medisas Code Challenge
+======================
+
+'''
+Use Twitter's streaming API to show the top 10 retweeted tweets in a rolling window of time, where the window's start is n minutes ago (where n is defined by the user) and the window's end is the current time.
+'''
+
+#Requirements
+
+python 2.x
+virtualenv
+mongodb (default settings)
+
+#Setup Part 1: Twitter API Credentials
+
+1. Go to dev.twitter.com and set up an app
+2. edit the ```retweet_steam.py``` file and fill in the necessary Twitter API values
+
+#Setup Part 2: MongoDB
+
+1. install MongoDB (see http://www.mongodb.org/downloads for more details)
+2. Start mongoDB on your computer with the default configuration
+
+#Setup Part 3: Python Environment
+
+1. create a new Python virutalenv
+2. activate your virtualenv and use it from here on
+3. install the listed packages in requirements.txt ```pip install -r requirements.txt```
+
+#Running the code
+
+1. from your terminal run ```python retweet_stream.py```
+2. Type into the console a single number follow by the return key.
+
+#Notes
+
+When you first start retweet_stream.py a background process will start scraping
+the Twitter API (statuses/sample.json&filter_level=medium). If the scraper sees 
+a tweet with retweet_count > 0 it saves that tweet into MongoDB. However, in my
+brief time testing the scraper, I rarely ever see tweets with retweet_count > 0.
+I'd guess that 99% of tweets are not retweeted. I can't seem to find the
+streaming API that allows me to filter by retweets.
---
+++ retweet_stream.py
@@ -0,0 +1,68 @@
+import sys
+import twitter 
+import pymongo
+import datetime
+from multiprocessing import Process
+
+OAUTH_TOKEN = ''
+OAUTH_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+
+
+def print_top_retweets(mins_ago):
+    print 'Here are the top 10 retweets from the last %d minutes' % mins_ago
+    
+    client = pymongo.MongoClient()
+    db = client.medisas_challenge
+    collection = db.retweets
+
+    mins_ago_dt = datetime.datetime.utcnow() -\
+        datetime.timedelta(minutes=mins_ago)
+
+    res = collection.find({"created_at": {"$gte": mins_ago_dt}})\
+            .sort("retweet_count", pymongo.DESCENDING)\
+            .limit(10)
+
+    for index, item in enumerate(res):
+        print '%d. # Retweets: %d tweet: "%s"' % (index+1,
+            item['retweet_count'], item['text'])
+
+
+def scrape_twitter():
+    auth = twitter.OAuth(OAUTH_TOKEN, OAUTH_SECRET,
+        CONSUMER_KEY, CONSUMER_SECRET)
+    stream = twitter.TwitterStream(auth=auth)
+    samples = stream.statuses.sample(filter_level='medium')
+
+    client = pymongo.MongoClient()
+    db = client.medisas_challenge
+    collection = db.retweets
+
+    for tweet in samples:
+        if tweet['retweet_count'] > 0:
+            #save to DB because it has been retweeted atleast once
+            created_at = datetime.datetime.strptime(tweet['created_at'],
+                '%a %b %d %X +0000 %Y')
+            text = tweet['text']
+            retweet_count = tweet['retweet_count']
+
+            data = {'text': text, 'retweet_count': retweet_count,
+                'created_at': created_at}
+
+            collection.insert(data)
+
+if __name__ == '__main__':
+    scraper = Process(target=scrape_twitter)
+    scraper.start()
+
+    print "Enter a number X to see the top 10 retweets in the past X minutes"
+
+    while sys.stdin:
+        num = raw_input()
+        try:
+            mins_ago = int(num)
+            print_top_retweets(mins_ago)
+        except:
+            pass
+
